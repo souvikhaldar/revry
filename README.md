fe254fea6630fb900a9b420d29b163ac3aee3a34
Souvik Haldar <souvikhaldar@Souviks-MacBook-Air.local>
2024-04-27 14:10:31 +0530

    Add period of a week support

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 It creates a one page content of all the journal entries for the given month. 
 
 # roadmap
-Add support for:  
-1. weekly  
-2. custom  
-date range for creating the content
+1. Add support for:  
+    1. weekly  [DONE]
+    2. custom  
+    date range for creating the content  
 
-Add chatGPT support for creating summary from the content.  
+2. Add chatGPT support for creating summary from the content.   
 
--- main.go
+++ main.go
@@ -5,18 +5,42 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
 	path := flag.String("path", ".", "path to read the diary files")
+	period := flag.String("period", "week", "time period of collecting journal entries")
 	flag.Parse()
+	currTime := time.Now()
+	var limit time.Time
+	if *period == "week" {
+		limit = currTime.Add(-7 * 24 * time.Hour)
+	} else {
+		limit = currTime.Add(-31 * 24 * time.Hour)
+	}
+
+	layout := "02-01-2006"
 
 	files, err := os.ReadDir(*path)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	var introspection string
 	for _, file := range files {
+		fileName := file.Name()
+		fileName = strings.TrimSuffix(fileName, ".md")
+		// Parse the string into a time.Time object
+		parsedTime, err := time.Parse(layout, fileName)
+		if err != nil {
+			fmt.Println("Error parsing time:", err)
+			return
+		}
+		if parsedTime.Before(limit) {
+			continue
+		}
 		content, err := os.ReadFile(file.Name())
 		if err != nil {
 			log.Fatal(err)
